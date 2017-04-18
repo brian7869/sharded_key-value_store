0154267240bd1a2e22760e493059c72eda800c3c
brian7869 <weichih@umich.edu>
2017-04-17 20:44:50 -0400

    modify request format to accomadate multiple-client case

--- request_generator.py
+++ request_generator.py
@@ -3,19 +3,20 @@ import random, sys
 prob_of_get = 0.4
 prob_of_put = 0.4
 
-def random_request(seq):
+def random_request(client_id, seq):
 	random_for_type = random.random()
 	if random_for_type < prob_of_get:
-		return 'Get {} {}\n'.format(random.randint(100,109), seq)
+		return 'Get {} {}_{}\n'.format(random.randint(100,109), client_id, seq)
 	elif random_for_type >= prob_of_get and random_for_type < prob_of_get + prob_of_put:
-		return 'Put {} {}\n'.format(random.randint(100,109), seq)
+		return 'Put {} {}_{}\n'.format(random.randint(100,109), client_id, seq)
 	else:
-		return 'Delete {} {}\n'.format(random.randint(100,109), seq)
+		return 'Delete {} {}_{}\n'.format(random.randint(100,109), client_id, seq)
 	seq += 1
 
 if __name__ == '__main__':
 	num_request = int(sys.argv[1])
-	file_name = sys.argv[2]
+	client_id = int(sys.argv[2])
+	file_name = sys.argv[3]
 	with open(file_name, 'w') as f:
 		for i in xrange(num_request):
-			f.write(random_request(i))
+			f.write(random_request(client_id, i))
